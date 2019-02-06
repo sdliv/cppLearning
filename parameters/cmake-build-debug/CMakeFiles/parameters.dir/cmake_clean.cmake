d7b8c019a51d07e22031b81d8d816cc31ff3c609
Sean <sean@covertalert.ai>
2019-02-06 10:34:15 -0500

    Functions with parameters

---
+++ parameters/cmake-build-debug/CMakeFiles/parameters.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parameters.dir/main.cpp.o"
+  "parameters.pdb"
+  "parameters"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parameters.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ parameters/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void printValueDoubled(int x) {
+    std::cout << x << " doubled is: " << x * 2 << '\n';
+}
+
+int getValueFromUser() {
+    std::cout << "Enter a value: ";
+
+    int x { 0 };
+    std::cin >> x;
+
+    return x;
+}
+
+int main() {
+
+    int x { getValueFromUser() };
+    printValueDoubled(x);
+
+    return 0;
+}
